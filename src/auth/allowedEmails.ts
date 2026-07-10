63306a2ebe3c8dd6bbcbe232b723d31e96af30ea
HMS-4 <info@harshubh.com>
2026-07-10 10:02:25 +0530

    new

--- src/auth/allowedEmails.ts
+++ src/auth/allowedEmails.ts
@@ -3,5 +3,8 @@ export const allowedEmails = [
   "rupamdholakiya023@gmail.com",
   "jdholakiya04@gmail.com",
   "info@harshubh.com",
-  "class@harshubh.com"
+  "class@harshubh.com",
+
+  // 10/07/2026 09/07/27"
+  "astro@harshubh.com"
 ];
